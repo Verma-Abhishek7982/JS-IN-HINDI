6b62d5a3b9932976058470a6339594950ceed857
Verma-Abhishek7982 <abhi26112003@gmail.com>
2024-11-17 11:02:57 +0000

    dataType

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,30 @@
+"use strict"; // treat all JS code as newer version
+
+// alert( 3 + 3) // we are using nodejs, not browser
+
+console.log(3 
+    +
+     3) // code readability should be high dont use this writing style
+
+console.log("Abhishek")
+
+
+let name = "Abhishek" //string dataType
+let age = 21 // int dataType
+let isLoggedIn = false // booleans dataType
+let state; // undefined dataType
+
+// dataTypes =>
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique
+
+
+// object
+// using typeof dataType to check the type of the variable or values in js
+console.log(typeof undefined); // as its dataType is also undefined
+console.log(typeof null); // but its DataType is an object
