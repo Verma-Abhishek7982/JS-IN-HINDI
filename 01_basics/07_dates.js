23ae8c600c129a8aeebbbeed7742701d20b0a747
Verma-Abhishek7982 <abhi26112003@gmail.com>
2024-11-19 13:03:36 +0000

    new changes

--- 01_basics/07_dates.js
+++ 01_basics/07_dates.js
@@ -1,13 +1,13 @@
 // Dates
 
 let myDate = new Date() // new date() function used to give the date with time
-console.log(myDate)
-console.log(myDate.toString());
-console.log(myDate.toDateString());
-console.log(myDate.toLocaleString());
-console.log(myDate.toLocaleDateString());
-console.log(myDate.toJSON());
-console.log(typeof myDate);
+// console.log(myDate)
+// console.log(myDate.toString());
+// console.log(myDate.toDateString());
+// console.log(myDate.toLocaleString());
+// console.log(myDate.toLocaleDateString());
+// console.log(myDate.toJSON());
+// console.log(typeof myDate);
 
 // let myCreatedDate = new Date(2023, 0, 23)
 // let myCreatedDate = new Date(2023, 0, 23, 5, 3)
@@ -15,11 +15,11 @@ console.log(typeof myDate);
 let myCreatedDate = new Date("01-14-2023")
 // console.log(myCreatedDate.toLocaleString());
 
-let myTimeStamp = Date.now()
+let myTimeStamp = Date.now() // give time is milliseconds since 1st january 1970 till now
 
 console.log(myTimeStamp);
 // console.log(myCreatedDate.getTime());
-// console.log(Math.floor(Date.now()/1000));
+// console.log(Math.floor(Date.now()/1000)); // give time in seconds by dividing it by 1000
 
 let newDate = new Date()
 console.log(newDate);
