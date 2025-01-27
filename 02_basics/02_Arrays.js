d5c5ce77b0bf264055feeb88888a065c7c46f9a3
Verma-Abhishek7982 <abhi26112003@gmail.com>
2025-01-27 16:31:08 +0000

    new changes

--- 02_basics/02_Arrays.js
+++ 02_basics/02_Arrays.js
@@ -12,7 +12,7 @@ console.log(allHeros);
 const all_new_heros = [...marvel_heros, ...dc_heros] //Spreading in array just as concatenate 
 // spread mean glass fek dia => (drop drop drop) same by using ...
 
-// console.log(all_new_heros);
+console.log(all_new_heros);
 
 const another_array = [1, 2, 3, [4, 5, 6], 7, [6, 7, [4, 5]]]
 
@@ -21,12 +21,13 @@ console.log(real_another_array);
 
 
 
-console.log(Array.isArray("Hitesh"))
-console.log(Array.from("Hitesh"))
-console.log(Array.from({name: "hitesh"})) // interesting
+console.log(Array.isArray("Abhishek"))
+console.log(Array.from("Abhishek"))
+console.log(Array.from({name: "Abhishek"})) // interesting here gives the empty array as using object
 
 let score1 = 100
 let score2 = 200
 let score3 = 300
 
 console.log(Array.of(score1, score2, score3));
+// using array.from or array.of is same as both making any variable, string etc => array
